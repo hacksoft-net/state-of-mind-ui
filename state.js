b0fac32329f950cca40b2fcef1c83e8a96a376b0
hacksoft-net <216828489+hacksoft-net@users.noreply.github.com>
2025-06-22 19:41:28 +0900

    Update state.js for stately subscription

--- state.js
+++ state.js
@@ -1,14 +1,36 @@
+// SOM API - Stately
 export class Stately {
   constructor() {
+    this.SubscribedFunctions = {};
     this.StateInformation = {};
     this.State = null;
   }
 
   createState(name, defaultValue = "?") {
-    Object.defineProperty(this.StateInformation, name, {value: defaultValue, writable: true}); 
+    const writableInstance = {value: defaultValue, writable: true};
+    Object.defineProperty(this.StateInformation, name, writableInstance); 
+  }
+
+  createFunction(targetStateField, callback) {
+    const writableInstance = {value:[], writable: true}
+    if (!this.SubscribedFunctions[targetStateField]) {
+      Object.defineProperty(this.SubscribedFunctions, targetStateField, writableInstance);
+      this.SubscribedFunctions[targetStateField].push(callback);
+    } else { // There is a callback array for this goofy  
+      this.SubscribedFunctions[targetStateField].push(callback);
+    }
   }
 
   initializeState() {
-    this.State = new Proxy();
-  } 
+    const handler = {
+      get(target, prop, receiver) {
+        this.SubscribedFunctions[prop].forEach((element) => element());
+      },
+      set(target, prop, receiver) {
+        this.SubscribedFunctions[prop].forEach((element) => element());
+      }
+    }
+    
+    this.State = new Proxy(this.StateInformation, handler);
+  }
 }
